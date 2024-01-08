f3aaf42076c251e9a35e92610b9af07be9ec06a2
aBugInTheSystem <ekom338.com@gmail.com>
2024-01-09 00:37:43 +0100

    Page animation transitions

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,29 @@
+<template>
+  <div class="bg-gray-200 min-h-screen overflow-y-hidden scroll-smooth relative">
+    <Navbar />
+    <RouterView v-slot="{ Component }">
+      <transition name="fade" mode="out-in">
+        <component :is="Component" />
+      </transition>
+    </RouterView>
+    <Footer />
+  </div>
+</template>
+
 <script setup>
 import { RouterView } from 'vue-router';
 import Navbar from './components/Navbar.vue';
 import Footer from './components/Footer.vue';
 </script>
 
-<template>
-  <div class="bg-gray-200 min-h-screen overflow-y-hidden scroll-smooth relative">
-    <Navbar />
-    <RouterView />
-    <Footer />
-  </div>
-</template>
+<style>
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
 
-
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease-in-out;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,27 +1,30 @@
 
 <template>
-  <transition name="slide-fade">
-    <main v-show="pageLoaded"
-      class="px-3 flex flex-col-reverse md:flex md:items-center md:flex-row md:justify-center md:mt-36 ">
-      <div class=" flex justify-center md:flex-none">
-        <img src="/Me.jpeg" alt="" class="rounded-full mt-10 md:w-[400px] pr-3 ">
-      </div>
-      <div class="mt-10 flex flex-col md:pl-7 ">
-        <h1 class="text-4xl font-Josefin font-[500] md:text-5xl">Hey there <span class="wave">👋🏿</span>
-        </h1>
-        <p class="text-xl mt-3 pr-3 font-Josefin w-[370px] md:text-2xl md:w-[500px] md:pr-0 "> I'm Ekomobong, a passionate
-          web
-          developer
-          with a flair
-          for
-          crafting seamless digital experiences. From front-end finesse to back-end brilliance, I bring a holistic
-          approach
-          to web development.</p>
-      </div>
-    </main>
-  </transition>
-  <About v-motion-slide-visible-once-right />
-  <Technology v-motion-slide-visible-once-right />
+  <div>
+    <transition name="slide-fade">
+      <main v-show="pageLoaded"
+        class="px-3 flex flex-col-reverse md:flex md:items-center md:flex-row md:justify-center md:mt-36 ">
+        <div class=" flex justify-center md:flex-none">
+          <img src="/Me.jpeg" alt="" class="rounded-full mt-10 md:w-[400px] pr-3 ">
+        </div>
+        <div class="mt-10 flex flex-col md:pl-7 ">
+          <h1 class="text-4xl font-Josefin font-[500] md:text-5xl">Hey there <span class="wave">👋🏿</span>
+          </h1>
+          <p class="text-xl mt-3 pr-3 font-Josefin w-[370px] md:text-2xl md:w-[500px] md:pr-0 "> I'm Ekomobong, a
+            passionate
+            web
+            developer
+            with a flair
+            for
+            crafting seamless digital experiences. From front-end finesse to back-end brilliance, I bring a holistic
+            approach
+            to web development.</p>
+        </div>
+      </main>
+    </transition>
+    <About v-motion-slide-visible-once-right />
+    <Technology v-motion-slide-visible-once-right />
+  </div>
 </template>
 
 
