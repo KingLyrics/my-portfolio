44607a7e94af15461fde6a8ee3c15de212cf7886
aBugInTheSystem <ekom338.com@gmail.com>
2024-01-09 12:37:10 +0100

    Resume completed

--- src/data.js
+++ src/data.js
@@ -9,10 +9,22 @@ const data = [
     feedback to elevate code quality and maintainability.`,
   },
   {
+    timeWorked: "Jan 2024 - Present",
+    role: "Frontend Developer Intern",
+    company: "InternPulse",
+    desc: `During my internship as a frontend developer, I focused on HTML and CSS development to structure and style web pages. Ensuring responsive design, I optimized applications for various devices. JavaScript was employed to enhance user interactivity, and I collaborated with frameworks like React and Vue for dynamic interfaces.`,
+  },
+  {
     timeWorked: "May 2023 - Jun 2023",
     role: "Web Developer Intern",
     company: "PromptBroker",
-    desc: "Engaged in frontend projects, collaborated with senior developers, and contributed to responsive and visually appealing web pages. Participated in code reviews and provided feedback for code quality and maintainability. ",
+    desc: "Engaged in frontend projects, collaborated with senior developers, and contributed to responsive and visually appealing web pages. Participated in code reviews and provided feedback for code quality and maintainability. Additionally, worked on  cross-browser compatibility issues and contributed to bug fixing, working collaboratively with UX/UI designers and utilizing version control systems like Git for efficient code management. ",
+  },
+  {
+    timeWorked: "March 2022 - Sep 2022",
+    role: "Network Engineer Intern",
+    company: "Comternet World Ltd",
+    desc: ` I actively participated in the configuration and maintenance of network infrastructure. I gained practical experience in implementing and troubleshooting network protocols, ensuring the seamless flow of data. Collaborating with senior engineers, I contributed to the optimization of network performance and assisted in resolving connectivity issues. This internship provided valuable insights into network security practices, and I engaged in tasks related to monitoring and analyzing network traffic. `,
   },
 ];
 
--- src/views/Resume.vue
+++ src/views/Resume.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="max-w-sm mx-auto font-Josefin lg:max-w-4xl  ">
+  <div class="max-w-sm mx-auto font-Josefin lg:max-w-4xl">
     <h1 class="text-3xl font-Noto my-10">Resume</h1>
-    <div class="md:flex items-center justify-between md:space-x-48">
+    <div class="md:flex items-center justify-between md:space-x-48 border border-b-black">
       <div>
-        <h2 class="text-3xl pb-9">Work <br> Experience</h2>
+        <h2 class="text-3xl pb-9" v-motion-pop-visible>Work <br> Experience</h2>
       </div>
-      <div class="">
+      <div class=" pb-20">
         <div class="mt-5 pb-3" v-for="(data, index) in data" :key="index">
           <h1 class="text-2xl my-2 text- font-Roboto">
             {{ data.timeWorked }}
@@ -17,8 +17,32 @@
         </div>
       </div>
     </div>
+    <div class="md:flex mt-12 items-center md:space-x-48 pb-10">
+      <div class="pb-10">
+        <h2 class="text-3xl pb-4" v-motion-pop-visible>Skills <br> & Expertise</h2>
+        <h2 class="text-xl pb-2">Tech Toolbox 🧰</h2>
+
+        <div class="mt-10">
+          <ul class="list-disc space-y-10">
+            <li class="lg:text-xl text-lg">Frontend Technologies: HTML, CSS, JavaScript (React, Vue), TailWindCSS,
+              BootStrap 5, Axios
+            </li>
+            <li class="lg:text-xl text-lg">Backend: Node.js, Express
+            </li>
+            <li class="lg:text-xl text-lg">Databases: MongoDB, MySQL, Postgresql
+            </li>
+            <li class="lg:text-xl text-lg">Tools: Git, VS Code
+            </li>
+            <li class="lg:text-xl text-lg">Other Skills: Responsive Design, API Integration, Problem Solving
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+<!-- Frontend: 
+-->
 
 <script setup>
 import data from '@/data';
