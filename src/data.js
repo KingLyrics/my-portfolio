b35fd0be71f27717a639420d3f40c5dee747e166
aBugInTheSystem <ekom338.com@gmail.com>
2024-01-09 03:25:52 +0100

    Data

---
+++ src/data.js
@@ -0,0 +1,19 @@
+const data = [
+  {
+    timeWorked: "Sep 2023 - Present",
+    role: "Frontend Developer Intern",
+    company: "IEnergyUp-InnovationLab",
+    desc: `Engaged with the senior development team to grasp project requirements, delivering responsive
+    and visually
+    appealing web pages for an enhanced user experience. Participated in code reviews, providing constructive
+    feedback to elevate code quality and maintainability.`,
+  },
+  {
+    timeWorked: "August",
+    role: "Web Developer Intern",
+    company: "PromptBroker",
+    desc: "Engaged in frontend projects, collaborated with senior developers, and contributed to responsive and visually appealing web pages. Participated in code reviews and provided feedback for code quality and maintainability. ",
+  },
+];
+
+export default data;
--- src/views/Resume.vue
+++ src/views/Resume.vue
@@ -1,5 +1,26 @@
 <template>
-  <div>
-    Resume
+  <div class="max-w-sm mx-auto font-Josefin lg:max-w-4xl  ">
+    <h1 class="text-3xl font-Noto my-10">Resume</h1>
+    <div class="md:flex items-center justify-between md:space-x-48">
+      <div>
+        <h2 class="text-3xl pb-9">Work <br> Experience</h2>
+      </div>
+      <div class="">
+        <div class="mt-5" v-for="(data, index) in data" :key="index">
+          <h1 class="text-2xl my-2 text- font-Roboto">
+            {{ data.timeWorked }}
+          </h1>
+          <h1 class="text-xl">{{ data.role }} <br>@{{ data.company }}</h1>
+          <p class="mt-5">
+            {{ data.desc }}
+          </p>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+
+<script setup>
+import data from '@/data';
+
+</script>
